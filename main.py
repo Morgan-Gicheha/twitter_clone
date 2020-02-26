17f934a2ad711fe58d77a84101eab49e9bf40688
codeGiche <morgangicheha4@gmail.com>
2020-02-26 12:02:50 +0300

    initial commit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def create():
 POSTGRES_LINK="postgresql://postgres:morgan8514@127.0.0.1:5432/twitter_clone"
 HEROKU_LINK= 'postgres://kbiumvmlmgywyy:ab6cdb841d615b28942dda992221111bba8d679673d6dc74ef941f7ada060875@ec2-18-213-176-229.compute-1.amazonaws.com:5432/dcfgerp8p1hj70'
 # configuring database
-app.config["SQLALCHEMY_DATABASE_URI"]=HEROKU_LINK
+app.config["SQLALCHEMY_DATABASE_URI"]=POSTGRES_LINK
 app.config["SECRET_KEY"]= "secret"
 app.config["DEBUG"]=True
 # configuring uploads
